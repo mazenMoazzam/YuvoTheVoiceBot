41106935d852fe6298bee6770de794593fd1dcf7
Mazen Moazzam <mm5622@drexel.edu>
2024-07-17 01:09:01 -0400

    Set up database.cs for postgres database for the reminder system of the bot.

--- obj/Debug/net8.0/project.AssemblyInfo.cs
+++ obj/Debug/net8.0/project.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("project")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d04479fe3dd3ef32edd55aeda8ec246132840505")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5818ab1591de125bcd014cd71fdcc2582756354f")]
 [assembly: System.Reflection.AssemblyProductAttribute("project")]
 [assembly: System.Reflection.AssemblyTitleAttribute("project")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
